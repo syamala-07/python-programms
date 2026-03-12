17c5ba83c9a738638165cbe409ad80d1844ce06d
MIDDE SYAMALA <middesyamala07@gmail.com>
2026-03-12 12:13:22 +0530

    INDIAN TO LONDAN TIME CONVERTER
    Concepts used:
    input()
    arithmetic
    integer division //
    modulus %

---
+++ Indian to London TIme.py
@@ -0,0 +1,6 @@
+h = int(input("Enter hour in India: "))
+m = int(input("Enter minutes in India: "))
+total_minutes = h*60 + m - 330   # 330 minutes = 5 hours 30 minutes
+london_hour = (total_minutes // 60) % 24
+london_minute = total_minutes % 60
+print(f"London Time: {london_hour}:{london_minute}")
